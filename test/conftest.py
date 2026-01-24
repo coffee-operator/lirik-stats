a2c660676b170798fbaf6de71216d5421d897b71
coffee-operator <jjlx789@gmail.com>
2026-01-24 13:28:21 -0700

    test: add coverage for main.py

--- test/conftest.py
+++ test/conftest.py
@@ -1,5 +1,7 @@
+from datetime import datetime, timezone
 from pathlib import Path
 from typing import Callable
+import uuid
 from pytest import fixture
 from unittest.mock import MagicMock
 from google.oauth2.service_account import Credentials
@@ -8,6 +10,15 @@ from youtube.models import ChannelInfo, PlaylistItemsResponse
 from youtube.youtube_api import YouTubeAPI
 from youtube.youtube_client import YouTubeClient
 from youtube.youtube_service import YouTubeService
+from youtube.storage_service import (
+    api_sources,
+    data_stages,
+    data_sources,
+    StorageService,
+)
+
+
+# YouTubeClient/API/Service
 
 
 FAKE_SCOPES = ["www.youtubeapis.read.com"]
@@ -104,3 +115,68 @@ def f_playlist_items_response() -> Callable[[str, str], PlaylistItemsResponse]:
         )
 
     return _create
+
+
+# STORAGE SERVICE
+
+
+@fixture
+def m_channel_id() -> str:
+    return "123abc"
+
+
+@fixture
+def m_channel_folder_name() -> str:
+    return "alpha"
+
+
+@fixture
+def m_clock() -> datetime:
+    return datetime(2026, 1, 23, 12, 0, 0, tzinfo=timezone.utc)
+
+
+@fixture
+def m_uuid_provider() -> uuid:
+    return uuid.UUID("d8d2e059-bbb9-47c8-9907-c2b0bbdeaf1d")
+
+
+@fixture
+def m_base_data_path(tmp_path) -> Path:
+    return tmp_path
+
+
+@fixture
+def m_api_source() -> api_sources:
+    return "youtube_api"
+
+
+@fixture
+def m_data_stage() -> data_stages:
+    return "raw"
+
+
+@fixture
+def m_data_source() -> data_sources:
+    return "workflow"
+
+
+@fixture
+def m_json_gz_file_name(m_clock, format="%Y-%m-%d") -> str:
+    return f"{m_clock.strftime(format)}.json.gz"
+
+
+@fixture
+def m_storage_service(
+    m_channel_id,
+    m_channel_folder_name,
+    m_clock,
+    m_uuid_provider,
+    m_base_data_path,
+) -> StorageService:
+    return StorageService(
+        channel_id=m_channel_id,
+        channel_folder_name=m_channel_folder_name,
+        clock=lambda: m_clock,
+        uuid_provider=lambda: m_uuid_provider,
+        base_data_path=m_base_data_path,
+    )
---
+++ test/test_main.py
@@ -0,0 +1,114 @@
+from unittest.mock import patch
+from youtube.cli import parse_cli_args
+from youtube.models import MainCliArgs
+from youtube import config
+from main import main, run
+
+
+def test_run(
+    f_youtube_service,
+    m_youtube_api,
+    f_channel_info,
+    f_playlist_items_response,
+    m_storage_service,
+    playlist_id: str = "123123123",
+):
+    # Arrange
+    ## cli args
+    args = MainCliArgs(
+        key_file_path=config.DEFAULT_KEY_FILE_PATH,
+        channel_id=config.DEFAULT_CHANNEL_ID,
+        channel_folder_name=config.DEFAULT_CHANNEL_FOLDER_NAME,
+    )
+
+    ## YouTubeService
+    youtube_service = f_youtube_service(youtube_api=m_youtube_api)
+
+    ### Mock YouTubeService internal calls
+    m_youtube_api.get_channel_info.return_value = f_channel_info(
+        channel_id=args.channel_id, playlist_id=playlist_id
+    )
+
+    page_1 = f_playlist_items_response(playlist_id=playlist_id, next_page_token="1")
+    page_2 = f_playlist_items_response(playlist_id=playlist_id, next_page_token="2")
+    page_3 = f_playlist_items_response(playlist_id=playlist_id, next_page_token=None)
+    m_youtube_api.get_channel_playlist_items.side_effect = [page_1, page_2, page_3]
+
+    ## StorageService
+    # fixture
+
+    # Act
+    run(args=args, youtube_service=youtube_service, storage_service=m_storage_service)
+
+    m_channel_file_path = m_storage_service._create_target_file_path(
+        api_source="youtube_api", data_stage="raw", data_source="channel"
+    )
+    m_video_file_path = m_storage_service._create_target_file_path(
+        api_source="youtube_api", data_stage="raw", data_source="video"
+    )
+    m_workflow_file_path = m_storage_service._create_target_file_path(
+        api_source="youtube_api", data_stage="raw", data_source="workflow"
+    )
+
+    # Assert
+    assert m_channel_file_path.exists()
+    assert m_video_file_path.exists()
+    assert m_workflow_file_path.exists()
+
+
+@patch("main.YouTubeClient")
+@patch("main.YouTubeAPI")
+@patch("main.YouTubeService")
+@patch("main.StorageService")
+@patch("main.run")
+def test_main_setup(
+    p_run,
+    p_storage_service,
+    p_youtube_service,
+    p_youtube_api,
+    p_youtube_client,
+    key_file_path: str = str(config.DEFAULT_KEY_FILE_PATH),
+    channel_id: str = "123123",
+    channel_folder_name: str = "lirik_wins",
+):
+    # Strategy is to test the wiring between my Objects, not the business logic handled by run(),
+    # since that's addressed above
+
+    # Arrange
+    cli_args = [
+        "--key_file_path",
+        key_file_path,
+        "--channel_id",
+        channel_id,
+        "--channel_folder_name",
+        channel_folder_name,
+    ]
+
+    # Act
+    main(cli_args=cli_args)
+    ## main internal call
+    args = parse_cli_args(cli_args=cli_args)
+
+    # Assert
+    assert args.key_file_path == key_file_path
+    assert args.channel_id == channel_id
+    assert args.channel_folder_name == channel_folder_name
+
+    p_youtube_client.assert_called_once_with(
+        key_file_path=key_file_path,
+        scopes=config.SCOPES,
+        api_service_name=config.API_SERVICE_NAME,
+        api_version=config.API_VERSION,
+        cache_discovery=config.CACHE_DISCOVERY,
+    )
+    p_youtube_api.assert_called_once_with(p_youtube_client.return_value)
+    p_youtube_service.assert_called_once_with(p_youtube_api.return_value)
+    p_storage_service.assert_called_once_with(
+        channel_id=channel_id, channel_folder_name=channel_folder_name
+    )
+
+    p_run.assert_called_once_with(
+        args=args,
+        youtube_service=p_youtube_service.return_value,
+        storage_service=p_storage_service.return_value,
+    )
